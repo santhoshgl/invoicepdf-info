ecc4bb111a66fb4557a50f5bf8990fd904a464e7
Santhosh GL <santhosh.gl@cilalabs.com>
2023-03-29 23:49:45 +0530

    Invoice PDF Generator

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import InvoicePage from './components/InvoicePage'
+import { Invoice } from './data/types'
+
+function App() {
+  const savedInvoice = window.localStorage.getItem('invoiceData')
+  let data = null
+
+  try {
+    if (savedInvoice) {
+      data = JSON.parse(savedInvoice)
+    }
+  } catch (_e) { }
+
+  const onInvoiceUpdated = (invoice: Invoice) => {
+    window.localStorage.setItem('invoiceData', JSON.stringify(invoice))
+  }
+
+  return (
+    <div className="app">
+      <InvoicePage data={data} onChange={onInvoiceUpdated} />
+    </div>
+  )
+}
+
+export default App
